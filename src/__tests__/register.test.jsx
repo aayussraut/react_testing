88e9c6097565b21a9cc84b9d894de2e5e65301a9
aayussraut <aayussraut.ar@gmail.com>
2023-06-29 10:52:14 +0545

    moved the test file to test folder

--- src/__tests__/register.test.jsx
+++ src/__tests__/register.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Register from "./Register.jsx";
+import Register from "../component/register/Register.jsx";
 import userEvent from "@testing-library/user-event";
 
 describe("Register Component", () => {
